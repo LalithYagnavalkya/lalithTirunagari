8d7cd1cb6f8760a276e124704a1e35cd0db1990f
Lalith <y.lalith01@gmail.com>
2024-02-12 08:53:39 +0530

    initialize navbar component

---
+++ src/components/navbar.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+    return <div className="container mx-auto pt-4">
+        <div>
+    
+        </div>
+        <Button>Download my resume</Button>        
+    </div>
+}
+
+export default Navbar;
