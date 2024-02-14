bb68f61405f94541d1b0d18d9449c226e360fd1d
Lalith <y.lalith01@gmail.com>
2024-02-14 08:48:03 +0530

    ⚡ Update Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,17 @@
+import Image from "next/image";
 import { Button } from "./ui/button";
 
 const Navbar = () => {
-    return <div className="container mx-auto pt-4">
-        <div>
-    
+    return <div className="container mx-auto pt-2 flex justify-between">
+        <div className="flex gap-x-4">
+            <Image src={"/images/logo-javascript.svg"} alt="js" width={"40"} height={"40"}></Image>
+            <span className="font-semibold">
+                Full Stack
+                <br />
+                Developer
+            </span>
         </div>
-        <Button>Download my resume</Button>        
+        <Button variant={'outline'}>Download my resume</Button>
     </div>
 }
 
